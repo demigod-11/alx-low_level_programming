ba4b154da9f93a40b1e30dd89343c74ca5f94711
demigod-11 <llousvamel11@gmail.com>
2022-12-09 18:32:13 +0100

    upper and low case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		putchar(n);
+	}
+	for (n = 65; n <= 90; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
