8a56c1fc06d4c4a20dc9c5f5fb87b3debe9714fd
demigod-11 <llousvamel11@gmail.com>
2022-12-13 18:46:11 +0100

    ia alpha

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a = 0;
+int b = 1;
+if (a > b)
+{
+  return (a);
+}
+return (0);
+}
