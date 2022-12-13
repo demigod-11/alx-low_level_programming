0d50c1cc76d36d3a69f61ca2466bf3cc4e200853
demigod-11 <llousvamel11@gmail.com>
2022-12-13 19:22:24 +0100

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_sign - check the code
+ *  *@c: letter being tested
+ * Return: Always 0.
+ */
+
+int _abs(int c)
+{
+	if (c > 0)
+	{
+		return (c * -1);
+	}
+	return (c);
+}
