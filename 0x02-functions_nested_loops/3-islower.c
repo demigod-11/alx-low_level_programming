4c73777753c4115e3e7f7d9ab928bac1d04bb12f
demigod-11 <llousvamel11@gmail.com>
2022-12-13 18:18:55 +0100

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _islower - check the code
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c < 97 && c >=122)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
