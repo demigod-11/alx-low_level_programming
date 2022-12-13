0608fcadf46a9b779dd5e98ca9f704173b7d91c5
demigod-11 <llousvamel11@gmail.com>
2022-12-13 18:24:27 +0100

    fixed bug

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,18 +3,18 @@
 #include <string.h>
 /**
  * _islower - check the code
- *
+ *  *@c: letter being tested
  * Return: Always 0.
  */
 
 int _islower(int c)
 {
-	if (c < 97 && c >=122)
+	if (c >= 97 && c <= 122)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }
