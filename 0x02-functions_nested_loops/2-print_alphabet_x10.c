a414beca7c21afe28c078bff92aa34c5fc344de7
demigod-11 <llousvamel11@gmail.com>
2022-12-13 17:57:57 +0100

    alphabet X10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_alphabet_x10 - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+
+		print_alphabet();
+	}
+	_putchar('\n');
+}
