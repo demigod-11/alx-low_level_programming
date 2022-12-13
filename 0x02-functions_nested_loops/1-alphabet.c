1afe11dc9fc71f022c20097b0172c567c9b5c7c1
demigod-11 <llousvamel11@gmail.com>
2022-12-13 17:45:37 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char word[] = "abcdefghijklmnopqrstuvwxyz"
+	int count = strlen(word);
+
+	for (i = 0; i < count; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
+}
