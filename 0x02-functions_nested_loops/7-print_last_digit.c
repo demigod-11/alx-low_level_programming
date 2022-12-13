b96f2d0ef2c084b5ba46418d33fc0933aa794128
demigod-11 <llousvamel11@gmail.com>
2022-12-13 19:36:56 +0100

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
+ */
+int print_last_digit(int c)
+{
+	if (c < 0)
+	{
+		c = -c;
+		return(c % 10);
+	}
+	return (c % 10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,4 +14,10 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_teble(void);
+int add(int, int);
+void print_to_98(int n);
+void print_time_table(int n);
 #endif
