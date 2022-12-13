2cec631ae521c1ca8e8f80a855d1d1364a1a9258
demigod-11 <llousvamel11@gmail.com>
2022-12-13 19:57:35 +0100

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,18 @@
  */
 int print_last_digit(int c)
 {
+
+	int a;
+
 	if (c < 0)
 	{
 		c = -c;
-		_putchar(c % 10);
-		return (c % 10);
 	}
-	return (c % 10);
+	a = c % 10;
+	if (a < 0)
+	{
+		a = -a;
+	}
+	_putchar(a + '0');
+	return (a);
 }
