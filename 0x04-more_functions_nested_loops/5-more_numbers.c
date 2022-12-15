b5183b745ae852b995256f0a68ab16f59005a914
demigod-11 <llousvamel11@gmail.com>
2022-12-15 18:55:46 +0100

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,17 @@
 void  more_numbers(void)
 {
 	int i;
+	int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (j = 0; j <= 14; j++)
 		{	
-			if (i > 9)
+			if (j >  9)
 			{
-				_putchar(i / 10 + '0');
+				_putchar(j / 10 + '0');
 			}
-			_putchat(i % 10 + '0');
+			_putchat(j % 10 + '0');
 		}
 		 _putchar('\n');
 	}
