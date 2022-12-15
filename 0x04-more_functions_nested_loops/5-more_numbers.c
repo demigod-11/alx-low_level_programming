83f588c4d0c3edfd2b0d52d9b02a186ad76cd518
demigod-11 <llousvamel11@gmail.com>
2022-12-15 17:50:03 +0100

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - Function print numbers from 0-14 *10
+ * Return: Void
+ */
+
+void  more_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i + '0');
+		}
+		 _putchar('\n');
+	}
+
+}
