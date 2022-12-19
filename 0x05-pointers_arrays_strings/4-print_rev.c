6461c156dc9aaab05e4ab66904c058a9ef8b9d3d
demigod-11 <llousvamel11@gmail.com>
2022-12-19 17:44:33 +0100

    rev str

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,16 +11,16 @@ void print_rev(char *s)
 {
 	int i, j;
 
-	i = 0
+	i = 0;
 
-	for (j = 0; str[j] != '\0'; j++)
+	for (j = 0; s[j] != '\0'; j++)
 	{
 		i++
 	}
 
 	for (j = 0; j < i; j++)
 	{
-		_putchar(str[j]);
+		_putchar(s[j]);
 	}
 	_putchar('\n');
 }
