c9faaa0e3c4ff870531618b283c16c521cc997fa
demigod-11 <llousvamel11@gmail.com>
2022-12-19 17:43:19 +0100

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_rev  - prints reverse of a  string and a new line.
+* @s: string to print (*char)
+* Return: void.
+*/
+
+void print_rev(char *s)
+{
+	int i, j;
+
+	i = 0
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		i++
+	}
+
+	for (j = 0; j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
