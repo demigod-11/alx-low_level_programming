6651355a3a76b38822f60cdbab1cd0e899e0832a
demigod-11 <llousvamel11@gmail.com>
2022-12-19 17:37:46 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts  - prints a string and a new line.
+* @str: string to print (*char)
+* Return: void.
+*/
+
+void _puts(char *str)
+{
+	int j;
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
