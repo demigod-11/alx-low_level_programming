173e12076db43effb391083bb89f0837ca0c8b80
demigod-11 <llousvamel11@gmail.com>
2022-12-19 17:01:15 +0100

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* reset_to_98  - updates the value of a pointer
+* @n: number to modify
+* Return: Always 0.
+*/
+
+void reset_to_98(int *n)
+{
+	int *p = &n;
+	*p = 98;
+}
+
