5ff64d2eeb53877aea1f776b5c1d20a86ca9388a
demigod-11 <llousvamel11@gmail.com>
2022-12-19 17:28:30 +0100

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strlen  - find the length of a string
+* @s: string (*char)
+* Return: length of string.
+*/
+
+int _strlen(char *s)
+{
+	int i,j;
+
+	i = 0;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		i++;
+	}
+
+	return (i);
+}
