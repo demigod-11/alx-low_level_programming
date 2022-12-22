bdc3e0b7f5a033fc4786326b66cedce34eabdecf
demigod-11 <llousvamel11@gmail.com>
2022-12-22 16:27:24 +0100

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: string.
+ * Return: string.
+ */
+
+char *cap_string(char *s)
+{
+	char *p;
+
+	if (*s >= 'a' && *s <= 'z')
+	{
+		*s = *s - 'a' + 'A';
+	}
+
+	for (p = s + 1; *p != '\0'; p++)
+	{
+		if (*p == ' ' || *p == '\t' || *p == '\n' || *p == ',' || *p == ';' || *p == '.' || *p == '!' || *p == '?' || *p == '"' || *p == '(' || *p == ')' || *p == '{' || *p == '}')
+		{
+			if (*(p + 1) >= 'a' && *(p + 1) <= 'z')
+			{
+				*(p + 1) = *(p + 1) - 'a' + 'A';
+			}
+		}
+	}
+	return (s);
+}
