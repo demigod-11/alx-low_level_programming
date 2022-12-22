725de74bf09f6e83d6f4dcf19a6be61d3f11aa1e
demigod-11 <llousvamel11@gmail.com>
2022-12-22 16:53:24 +0100

    @@

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,30 +7,30 @@
 
 char *leet(char *s)
 {
-	char *p;
+	int i;
+	int j;
+	int k;
 
-	for (p = s; *p != '\0'; p++)
+	char l[] = "oOlLeEaAtT";
+	char m[] = "0011334477";
+
+	i = 0;
+
+	while (s[i] != '\0')
 	{
-		if (*p == 'a' || *p == 'A')
+		j = 0;
+		k = 0;
+		while (l[j] != '\0')
 		{
-			*p = '4';
+			if (s[i] == l[j])
+			{
+				k = j;
+				s[i] = m[k];
+			}
+			j++;
 		}
-		else if (*p == 'e' || *p == 'E')
-		{
-			*p = '3';
-		}
-		else if (*p == 'o' || *p == 'O')
-		{
-			*p = '0';
-		}
-		else if (*p == 't' || *p == 'T')
-		{
-			*p = '7';
-		}
-		else if (*p == 'l' || *p == 'L')
-		{
-			*p = '1';
-		}
+		i++;
 	}
+
 	return (s);
 }
