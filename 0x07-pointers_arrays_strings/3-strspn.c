301b1c8c1e1b29260b1abcd48be28fc2dcb6e688
demigod-11 <llousvamel11@gmail.com>
2023-01-02 21:48:15 +0100

    alx coding assignment

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,22 +9,21 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int count = 0;
 	char *a = accept;
 
-	while (*s != '\0')
+	while (*s++)
 	{
-		while (*a != '\0')
+		while (*accept++)
 		{
 			if (*(s - 1) == *(accept - 1))
 			{
 				count++;
 				break;
 			}
-			a++;
 		}
-		if (*a == '\0')
+		if (!(*--accept))
 		{
 			break;
 		}
-		s++;
+		accept = a;
 	}
 	return (count);
 }
