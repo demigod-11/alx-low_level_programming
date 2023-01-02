e7c71c1cae8b6006fd69270a8bc35adf21d08261
demigod-11 <llousvamel11@gmail.com>
2023-01-02 21:02:07 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strchr - Function that locates a character in a string
+* @s: pointer to char
+* @c: char params to find
+* Return: *S
+*/
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return s;
+		}
+		s++;
+	}
+	return NULL;
+}
