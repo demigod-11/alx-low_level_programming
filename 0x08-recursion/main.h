03caf0d21331408b0db4d2dea2f28cb375ecd5e3
demigod-11 <llousvamel11@gmail.com>
2023-01-04 21:15:22 +0100

    Alx Coding Assignment Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,9 @@
-#ifdef main
+#ifndef main
 #define main
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-void _putchar(char c);
+int _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
