79a69f46de170cbda00c82e76c17551e42f328bf
demigod-11 <llousvamel11@gmail.com>
2023-01-09 21:00:56 +0100

    alx programme project task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the multiplication of two numbers, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the programarguments.
+ *
+ * Return: integer
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, k;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	k = num1 * num2;
+
+	printf("%d\n", k);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the program arguments.
+ *
+ * Return: Integer
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, k = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(arg[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		k += atoi(argv[i]);
+	}
+
+	printf("%d\n", k);
+
+	return (0);
+}
