655c020397d80176d53b7c5af5baa0b0b6893ae0
demigod-11 <llousvamel11@gmail.com>
2023-01-10 21:33:17 +0100

    alx-program project

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i, j, k, l;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		;
+	}
+
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		;
+	}
+
+	s = malloc(sizeof(char) * (i + j + 1));
+
+	if (s == NULL)
+		free(s);
+		return (NULL);
+
+	for (k = 0; k < i; k++)
+	{
+		s[k] = s1[k];
+	}
+
+	l = j;
+	for (j = 0; j <= l; k++, j++)
+	{
+		s[k] = s2[j];
+	}
+
+	return (s);
+}
