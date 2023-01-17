fe3f9dd2da6e012d436d745696e07e0f599a9943
demigod-11 <llousvamel11@gmail.com>
2023-01-17 19:10:38 +0100

    alx assignment

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - function that creates a new dog
+ * @name: name.
+ * @age: age
+ * @owner: owner
+ *
+ * Return: dog_t struct
+ */
+
+dog_t *new_dog(char *name, float age, char *owner) 
+{
+    dog_t *d = malloc(sizeof(dog_t));
+    if (d == NULL) {
+        return NULL;
+    }
+
+    d->name = malloc(strlen(name) + 1);
+    if (d->name == NULL) {
+        free(d);
+        return NULL;
+    }
+    strcpy(d->name, name);
+
+    d->age = age;
+
+    d->owner = malloc(strlen(owner) + 1);
+    if (d->owner == NULL) {
+        free(d->name);
+        free(d);
+        return NULL;
+    }
+    strcpy(d->owner, owner);
+
+    return d;
+}
