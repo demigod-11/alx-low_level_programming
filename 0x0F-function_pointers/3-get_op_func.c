aa93c2ce5352e9ec787970a355f64b91bf4ce9c9
demigod-11 <llousvamel11@gmail.com>
2023-01-22 18:51:28 +0100

    alx-low-level-programming

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * get_op_func - fuction to select the correct func
+ * @s: char
+ *
+ * Return:int
+ */
+int (*get_op_func(char *s))(int, int)
+{	
+	int i;
+
+	op_t ops[] = {
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div }
+	{ "%", op_mod },
+	{ NULL, NULL }
+	};
+
+	for (i = 0; i < 5; i++)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f)
+		};	
+	}
+
+	return (0);
+}
