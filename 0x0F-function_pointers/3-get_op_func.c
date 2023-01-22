84446ec96e74657a0f7772b1b791926e9601482a
demigod-11 <llousvamel11@gmail.com>
2023-01-22 20:12:29 +0100

    alx-low-level-programming

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,6 +10,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+	int i;
 	op_t ops[] = {
 	{ "+", op_add },
 	{ "-", op_sub },
@@ -18,12 +19,12 @@ int (*get_op_func(char *s))(int, int)
 	{ "%", op_mod },
 	{ NULL, NULL }
 	};
-	int i = 0;
+	i = 0;
 
 	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);	
+			return (ops[i].f);
 		i++;
 	}
 
