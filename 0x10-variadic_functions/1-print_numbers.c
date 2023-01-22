dbdecc23ece9e6689e70bbe45f121a193be4d4fe
demigod-11 <llousvamel11@gmail.com>
2023-01-22 16:48:05 +0100

    alx-low-level-programming

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,11 +12,12 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	int num;
+	unsigned int i;
 	va_list args;
 
 	va_start(args, n);
 
-	for (unsigned int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		num = va_arg(args, int);
 		printf("%d", num);
