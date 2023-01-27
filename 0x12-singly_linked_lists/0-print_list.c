8579e395b2320b170cd2e3859f1d1f861d6e7079
demigod-11 <llousvamel11@gmail.com>
2023-01-27 20:50:42 +0100

    alx lowlevel programming

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - ...
+ * @h: ...
+ *
+ * Return: ....
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+
