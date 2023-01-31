6ac1e24b0c10c84bdae61a0f6ec5c78abc13a7ef
demigod-11 <llousvamel11@gmail.com>
2023-01-31 20:36:09 +0100

    alx-low-level-programming

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * free_listint - ......
+ * @head: ....
+ *
+ * Description: .........
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
